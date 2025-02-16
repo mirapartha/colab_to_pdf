a7c2bf9157bbe25fe2813f439a81e512308d7c3e
Mira Partha <56740299+mirapartha@users.noreply.github.com>
2025-02-15 19:58:19 -0800

    Update colab_to_pdf.py

--- colab_to_pdf.py
+++ colab_to_pdf.py
@@ -15,7 +15,7 @@ Javascript(get_plotlyjs())
 
 result = subprocess.run(['jupyter', 'nbconvert', '--to', 'html', notebook_path, '--output', '/content'], capture_output=True, text=True, check=True) 
 
-notebook_filename = os.path.basename(PATH_TO_NOTEBOOK)  # Get filename from path
+notebook_filename = os.path.basename(notebook_path)  # Get filename from path
 html_filename = os.path.splitext(notebook_filename)[0] + '.html'
 pdf_filename = os.path.splitext(notebook_filename)[0] + '.pdf'
 print(f"HTML filename: {html_filename}")
